2da199e3c6e7d02736fd888bdb9ff15464db1c4f
G0nzalito <zavalagonzalo172@gmail.com>
2025-11-22 16:54:43 -0300

    Añadido de endpoint para la creación de sanciones y la consulta de todas las sanciones de 1 alquler

--- endpoints/routes/sanciones_bp.py
+++ endpoints/routes/sanciones_bp.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, jsonify, request
 from entities.tipoSancion import TipoSancion
 from entities.sancion import Sancion
+from db.base import Base
 
 sanciones_bp = Blueprint("sanciones", __name__, url_prefix="/api/sanciones")
 
@@ -41,5 +42,43 @@ def get_all_estados_sancion():
     try:
         estados = Sancion.get_all_estados_sanciones()
         return jsonify(estados), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+@sanciones_bp.route("/create", methods=["POST"])
+def create_sancion():
+    try:
+        data = request.get_json()
+        fecha = data.get("fecha")
+        id_tipo_sancion = data.get("id_tipo_sancion")
+        id_estado = data.get("id_estado")
+        costo_base = data.get("costo_base")
+        descripcion = data.get("descripcion")
+        id_alquiler = data.get("id_alquiler")
+
+        if not all([fecha, id_tipo_sancion, id_estado, costo_base, descripcion, id_alquiler]):
+             return jsonify({"error": "Missing required fields"}), 400
+
+        tipo_sancion = TipoSancion.get_tipo_sancion_by_id(id_tipo_sancion)
+        if not tipo_sancion:
+            return jsonify({"error": "TipoSancion not found"}), 404
+
+        # print("Tablas registradas en SQLAlchemy:", Base.metadata.tables.keys())
+
+        sancion = Sancion(fecha, id_tipo_sancion, id_estado, costo_base, descripcion, id_alquiler)
+        
+        # print(sancion.to_dict())
+
+        sancion.persist()
+
+        return jsonify({"message": "Sancion created successfully"}), 201
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+@sanciones_bp.route("/alquiler/<int:id_alquiler>", methods=["GET"])
+def get_sanciones_by_alquiler(id_alquiler):
+    try:
+        sanciones = Sancion.get_all_sanciones_of_alquiler(id_alquiler)
+        return jsonify(sanciones), 200
     except Exception as e:
         return jsonify({"error": str(e)}), 500
